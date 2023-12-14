d957b3727331bbe7de795fce404d5daefb35abec
Todor Diklich <todordiklich@gmail.com>
2023-12-14 15:54:11 +0200

    add React and Redux app

---
+++ React-projects/Redux and Typescript/src/store/hooks.ts
@@ -0,0 +1,12 @@
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from 'react-redux';
+
+import { AppDispatch, RootState } from './store';
+
+type DispatchFunction = () => AppDispatch;
+
+export const useCartDispatch: DispatchFunction = useDispatch;
+export const useCartSelentor: TypedUseSelectorHook<RootState> = useSelector;
